9063fbce4b57fc34c97cbc3e34da39d92640658c
greinisch <greinisch@youwol.com>
2022-07-28 19:25:00 +0200

    ✨ app-view => add carousel-3d with story's documentation

--- src/app/app.view.ts
+++ src/app/app.view.ts
@@ -1,13 +1,11 @@
-import {
-    Stream$,
-    VirtualDOM,
-    attr$,
-    childrenWithReplace$,
-} from '@youwol/flux-view'
+import { VirtualDOM, attr$, childrenWithReplace$ } from '@youwol/flux-view'
 import { AppState } from './app.state'
 import { TopBannerView } from './top-banner'
 import { DockableTabs } from '@youwol/fv-tabs'
 import { ContentView } from './content'
+import { Carousel3dView, CarouselSide } from './carousel-3d'
+import { BehaviorSubject } from 'rxjs'
+import { filter, take } from 'rxjs/operators'
 
 /**
  *
@@ -24,7 +22,74 @@ export class AppView implements VirtualDOM {
      * @group Immutable DOM Constants
      */
     public readonly class =
-        'h-100 w-100 d-flex flex-column fv-text-primary position-relative'
+        'h-100 w-100 d-flex flex-column fv-text-primary fv-bg-background'
+
+    /**
+     * @group Immutable DOM Constants
+     */
+    public readonly children: VirtualDOM[]
+
+    constructor(params: { appState: AppState }) {
+        Object.assign(this, params)
+        const selectedSide$ = new BehaviorSubject<CarouselSide>('front')
+        const carousel = new Carousel3dView({
+            frontView: new MainContentView({ appState: this.appState }),
+            rightView: {},
+            backView: {},
+            leftView: {
+                tag: 'iframe',
+                width: '100%',
+                height: '100%',
+                src: attr$(
+                    selectedSide$.pipe(
+                        filter((d) => d != 'front'),
+                        take(1),
+                    ),
+                    () => {
+                        return '/applications/@youwol/stories/latest?id=68e053dd-6f16-4481-b141-af9047f3096f'
+                    },
+                ),
+            },
+            selectedSide$: selectedSide$,
+        })
+        document.addEventListener('keydown', logKey)
+
+        function logKey(e) {
+            if (e.altKey && e.key == 'ArrowUp') {
+                e.preventDefault()
+                selectedSide$.next('front')
+            }
+            if (e.altKey && e.key == 'ArrowDown') {
+                e.preventDefault()
+                selectedSide$.next('left')
+            }
+        }
+        this.children = [
+            new TopBannerView({ appState: this.appState }),
+            {
+                class: 'w-100 flex-grow-1',
+                style: {
+                    minHeight: '0px',
+                },
+                children: [carousel],
+            },
+        ]
+    }
+}
+
+/**
+ * @category View
+ */
+export class MainContentView implements VirtualDOM {
+    /**
+     * @group States
+     */
+    public readonly appState: AppState
+
+    /**
+     * @group Immutable DOM Constants
+     */
+    public readonly class = 'h-100 w-100 d-flex'
 
     /**
      * @group Immutable DOM Constants
@@ -34,11 +99,9 @@ export class AppView implements VirtualDOM {
     /**
      * @group Immutable DOM Constants
      */
-    public readonly style: Stream$<
-        { [_key: string]: string },
-        { [_key: string]: string }
-    >
-
+    public readonly style = {
+        minHeight: '0px',
+    }
     constructor(params: { appState: AppState }) {
         Object.assign(this, params)
 
@@ -48,8 +111,8 @@ export class AppView implements VirtualDOM {
                 initialPanelSize: '300px',
             },
         })
+
         this.children = [
-            new TopBannerView({ appState: this.appState }),
             {
                 class: 'w-100 flex-grow-1 d-flex position-relative',
                 style: {
@@ -58,7 +121,8 @@ export class AppView implements VirtualDOM {
                 children: [
                     sideNavView,
                     {
-                        class: 'w-100 h-100',
+                        class: 'flex-grow-1 h-100 fv-bg-background',
+                        style: { minWidth: '0px' },
                         children: childrenWithReplace$(
                             this.appState.allProjectsState$,
                             (projectState) => {
@@ -67,7 +131,7 @@ export class AppView implements VirtualDOM {
                                         this.appState.selectedProject$,
                                         (project) =>
                                             project.id == projectState.id
-                                                ? 'h-100'
+                                                ? 'h-100 w-100'
                                                 : 'd-none',
                                     ),
                                     children: [
