e6ea6ae115a92cdbe91c23e9c14c1b3d9acf400f
youwol-guillaume <greinisch@youwol.com>
2022-10-05 22:23:15 +0200

    :recycle: add root index.ts

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+/**
+ * This module is the main entry point of the application, loaded at start and displaying a [[AppView]].
+ *
+ * @module Application
+ */
+export * from './auto-generated'
+export * from './app'
