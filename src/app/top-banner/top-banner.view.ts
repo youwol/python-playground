c9ddcefc39e076d9ade29c2205620d1ad19226ce
youwol-guillaume <greinisch@youwol.com>
2022-10-14 22:06:17 +0200

    :sparkles: top-banner => add 'help' action

--- src/app/top-banner/top-banner.view.ts
+++ src/app/top-banner/top-banner.view.ts
@@ -15,6 +15,12 @@ export class TopBannerView extends TopBannerBaseView {
                         innerText:
                             appState.assetInfo.fileInfo.metadata.fileName,
                     },
+                    {
+                        tag: 'a',
+                        href: '/applications/@youwol/stories/latest?id=9ad4ef57-182a-40a6-a5ba-17d0d3bbf47b&mode=reader',
+                        class: 'mx-3 fas fa-question fv-text-focus p-1 fv-pointer rounded fv-hover-bg-background-alt',
+                        target: '_blank',
+                    },
                 ],
             },
         })
