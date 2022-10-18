5d4beb7695b9faeac8d04f2ee9b0c15c37e02346
youwol-guillaume <greinisch@youwol.com>
2022-10-18 19:02:51 +0200

    :fire: code-editor => remove usage of 'Carousel' in code editor view

--- src/app/content/code-editor/utils.view.ts
+++ src/app/content/code-editor/utils.view.ts
@@ -1,10 +1,9 @@
-import { child$, VirtualDOM, HTMLElement$ } from '@youwol/flux-view'
+import { VirtualDOM, HTMLElement$ } from '@youwol/flux-view'
 import { ProjectState } from '../../project'
 import { CodeEditorView } from './code-editor.view'
-import { BehaviorSubject, fromEvent, merge } from 'rxjs'
-import { delay, filter, map } from 'rxjs/operators'
-import { Carousel3dView, CarouselSide } from '../../carousel-3d'
-import { OutputViewNode, SourceNode } from '../../explorer'
+import { BehaviorSubject } from 'rxjs'
+import { delay, filter } from 'rxjs/operators'
+import { CarouselSide } from '../../carousel-3d'
 
 /**
  * @category View
@@ -65,63 +64,6 @@ export class CodePageView implements VirtualDOM {
             ),
         })
 
-        const wrap$ = (i: number) => {
-            return child$(
-                merge(
-                    this.projectState.runStart$.pipe(
-                        map(() => 'start' as 'start'),
-                    ),
-                    params.projectState.createdOutputs$.pipe(
-                        filter((outputViews) => outputViews.length == i + 1),
-                    ),
-                ),
-                (outputViews) => {
-                    if (outputViews == 'start' || outputViews.length != i + 1)
-                        return {}
-                    return {
-                        class: 'h-100 w-100 overflow-auto',
-                        children: [outputViews[i].htmlElement],
-                    }
-                },
-            )
-        }
-        const carousel = new Carousel3dView({
-            frontView: codeEditorView,
-            rightView: wrap$(0),
-            backView: wrap$(1),
-            leftView: wrap$(2),
-            selectedSide$: selectedSide$,
-            options: {
-                transition: {
-                    duration: 0.5,
-                    ease: 0.2,
-                },
-            },
-        })
-        const getOutputViewIndex = (node) => {
-            if (node instanceof SourceNode) return -1
-            return this.projectState.explorerState
-                .getNode(this.sourcePath)
-                .resolvedChildren()
-                .findIndex((n) => n.id == node.id)
-        }
-        const sub0 = this.projectState.explorerState.selectedNode$
-            .pipe(
-                filter(
-                    (node) =>
-                        node instanceof OutputViewNode ||
-                        (node instanceof SourceNode &&
-                            node.path == this.sourcePath),
-                ),
-                map((node) => {
-                    return { 0: 'front', 1: 'right', 2: 'back', 3: 'left' }[
-                        getOutputViewIndex(node) + 1
-                    ]
-                }),
-            )
-            .subscribe((side: CarouselSide) => {
-                selectedSide$.next(side)
-            })
         this.children = [
             this.headerView,
             {
@@ -129,49 +71,8 @@ export class CodePageView implements VirtualDOM {
                 style: {
                     minHeight: '0px',
                 },
-                children: [carousel],
+                children: [codeEditorView],
             },
         ]
-
-        const next: Record<CarouselSide, CarouselSide> = {
-            front: 'right',
-            right: 'back',
-            back: 'left',
-            left: 'front',
-        }
-        const prev: Record<CarouselSide, CarouselSide> = {
-            front: 'left',
-            left: 'back',
-            back: 'right',
-            right: 'front',
-        }
-        const sub1 = fromEvent(document, 'keydown')
-            .pipe(
-                filter((ev: KeyboardEvent) => {
-                    return (
-                        (ev.altKey && ev.key == 'ArrowRight') ||
-                        (ev.altKey && ev.key == 'ArrowLeft')
-                    )
-                }),
-            )
-            .subscribe((ev) => {
-                const explorer = params.projectState.explorerState
-                const newSide =
-                    ev.key == 'ArrowRight'
-                        ? next[selectedSide$.getValue()]
-                        : prev[selectedSide$.getValue()]
-                const scriptNode = explorer.getNode(params.sourcePath)
-                const children = scriptNode.resolvedChildren()
-                if (newSide == 'front') explorer.selectedNode$.next(scriptNode)
-                if (newSide == 'right' && children[0])
-                    explorer.selectedNode$.next(children[0] as any)
-                if (newSide == 'back' && children[1])
-                    explorer.selectedNode$.next(children[1] as any)
-                if (newSide == 'left' && children[2])
-                    explorer.selectedNode$.next(children[2] as any)
-            })
-        this.connectedCallback = (htmlElement) => {
-            htmlElement.ownSubscriptions(sub0, sub1)
-        }
     }
 }
